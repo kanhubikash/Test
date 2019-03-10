c94cdd19421c9ba679a92ece94e0a0be1fd3925e
xyz@gmail.com <BISWA@192.168.1.17>
2019-03-11 00:17:29 +0530

    first

---
+++ springBootWelcome/src/main/java/com/biswa/demo/SpringBootWelcomeApplication.java
@@ -0,0 +1,13 @@
+package com.biswa.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWelcomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWelcomeApplication.class, args);
+	}
+
+}
---
+++ springBootWelcome/src/test/java/com/biswa/demo/SpringBootWelcomeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.biswa.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootWelcomeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
